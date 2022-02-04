8a132a2f5fe990ab6b00190c6b437c55176d523b
cerealthing1 <cerealthing1@outlook.com>
2022-02-03 22:24:00 -0500

    remove broken GLFW

--- TestGame/src/Main.cpp
+++ TestGame/src/Main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
-#include "glad/glad.h"
+#include "glad/glad.h" 
 #include "GLFW/glfw3.h"
 
 int main(void)
 {
-	std::cout << "Hello World" << std::endl;
-	std::cin.get();
-	return 0;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
--- premake5.lua
+++ premake5.lua
@@ -11,14 +11,17 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 IncludeDir = {}
 IncludeDir["GLFW"] = "TestGame/vendor/GLFW/include"
-IncludeDir["Glad"] = "TestGame/vendor/Glad/include"
 include "TestGame/vendor/GLFW"
+
+IncludeDir["Glad"] = "TestGame/vendor/Glad/include"
 include "TestGame/vendor/Glad"
 
 project "TestGame"
     location "TestGame"
     kind "ConsoleApp"
     language "C++"
+    cppdialect "C++17"
+    
 
     targetdir ("bin/" ..outputdir.. "/%{prj.name}")
 	objdir ("bin-int/" ..outputdir.. "/%{prj.name}")
@@ -29,28 +32,40 @@ project "TestGame"
         "%{prj.name}/src/**.cpp"
     }
 
+    defines{
+        "_CRT_SECURE_NO_WARNINGS"
+    }
+
     includedirs
     {
+        
         "%{IncludeDir.GLFW}",
-         "%{IncludeDir.Glad}"
+        "%{IncludeDir.Glad}"
     }
 
     links
 	{
 		"GLFW",
-        "GLAD",
+        "Glad",
 		"opengl32.lib"
 	}
 
+    filter "system:windows"
+        systemversion "latest"
+        defines
+        {
+            "GLFW_INCLUDE_NONE"
+        }
+
     filter {"system:windows", "configurations:Debug"}
-        cppdialect "Default"
+        cppdialect "C++17"
         staticruntime "On"
         systemversion "latest"
         defines "ISDEBUG"
         symbols "On"
 
     filter {"system:windows", "configurations:Release"}
-    cppdialect "Default"
+    cppdialect "C++17"
     staticruntime "On"
     systemversion "latest"
     defines "ISRELEASE"
