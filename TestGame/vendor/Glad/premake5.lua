dea2054b94a382bef25bdc0d954f8b6d962ee1ae
cerealthing1 <cerealthing1@outlook.com>
2022-02-03 20:30:01 -0500

    properly add GLFW :P

--- TestGame/vendor/Glad/premake5.lua
+++ TestGame/vendor/Glad/premake5.lua
@@ -3,7 +3,7 @@ project "Glad"
     language "C"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-inter/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
     files
     {
